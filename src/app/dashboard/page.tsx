658f9c2b9c0dba7141958057a6003c989a506dfe
Darrien Rushing <drushing.dev@gmail.com>
2025-05-02 23:32:38 -0500

    loading places from DB

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from 'next/link';
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 import DateIdeasList from "@/components/DateIdeasList";
 import { useAppStore } from '@/lib/clientStore';
@@ -11,6 +11,7 @@ import { useUser } from '@clerk/clerk-react';
 
 export default function Dashboard() {
     const [activeTab, setActiveTab] = useState("tab1")
+    const [places, setPlaces] = useState([]);
 
     const previewedItem = useAppStore((state: any) => state.previewedItem);
 
@@ -19,10 +20,20 @@ export default function Dashboard() {
     console.log(user)
     console.log(user?.emailAddresses[0].emailAddress)
 
+    useEffect(() => {
+        fetch('http://localhost:3000/api/places')
+            .then((res) => res.json())
+            .then((data) => {
+                setPlaces(data)
+            })
+    }, [])
+
     return (
         <>
             <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
                 <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+
+                    {/* {JSON.stringify(places)} */}
 
                     {user && isSignedIn ? (<div className="w-full max-w-2xl mx-auto p-4">
                         <Tabs value={activeTab} onValueChange={setActiveTab} className="w-full">
@@ -35,20 +46,21 @@ export default function Dashboard() {
                                     <DateIdeasList
                                         title="Places"
                                         placeholderText="Add a new date idea"
-                                        list={[
-                                            { id: '1', content: 'Picnic in the park', notes: 'picnic supplies... ...' },
-                                            { id: '2', content: 'Visit a museum', notes: 'directions to the museum' },
-                                            { id: '3', content: 'Cook dinner together', notes: 'we need to pick a recipe' },
-                                            { id: '4', content: 'Go stargazing', notes: 'where are the dark sky areas?' },
-                                            { id: '5', content: 'Take a dance class', notes: 'still need to decide on a studio' },
-                                        ]}
+                                        list={places}
+                                        // list={[
+                                        //     { id: '1', content: 'Picnic in the park', notes: 'picnic supplies... ...' },
+                                        //     { id: '2', content: 'Visit a museum', notes: 'directions to the museum' },
+                                        //     { id: '3', content: 'Cook dinner together', notes: 'we need to pick a recipe' },
+                                        //     { id: '4', content: 'Go stargazing', notes: 'where are the dark sky areas?' },
+                                        //     { id: '5', content: 'Take a dance class', notes: 'still need to decide on a studio' },
+                                        // ]}
                                     />
                                     <NotesDisplay notesContent={previewedItem} />
                                 </div>
                             </TabsContent>
                             <TabsContent value="tab2">
                                 <div className="flex flex-row justify-between align-center space-x-8">
-                                    <DateIdeasList
+                                    {/* <DateIdeasList
                                         title="Restaurants List"
                                         placeholderText="Add a new restaurant"
                                         list={[
@@ -56,7 +68,7 @@ export default function Dashboard() {
                                             { id: '2', content: 'Gibson Street Bar', notes: 'cool place on S. Lamar' },
                                             { id: '3', content: 'Odd Duck', notes: 'looks Austin fancy' },
                                         ]}
-                                    />
+                                    /> */}
                                     <NotesDisplay notesContent={previewedItem} />
                                 </div>
                             </TabsContent>
